356f3aab0e6752e6d50028a9ee523d37e9952491
Aditya Kumar <aditya30394@gmail.com>
2018-04-30 08:07:53 +0530

    An efficient method to check if a number is multiple of 3
    There is a pattern in binary representation of the number that
    can be used to find if number is a multiple of 3. If difference
    between count of odd set bits (Bits set at odd positions) and
    even set bits is multiple of 3 then is the number.
    
    Time Complexity is O(log n)
    
    GFG Link :
    https://www.geeksforgeeks.org/write-an-efficient-method-to-check-if-a-number-is-multiple-of-3/

--- C++/isMultipleOf3.cpp
+++ C++/isMultipleOf3.cpp
@@ -1,35 +1,44 @@
+/*
+Write an Efficient Method to Check if a Number is Multiple of 3
+
+Geeksforgeeks link - https://www.geeksforgeeks.org/write-an-efficient-method-to-check-if-a-number-is-multiple-of-3/
+*/
+
 #include <iostream>
-
-
 using namespace std;
 
 int isMultipleOf3(int n){
-	if(n<0)
-	n=-n;
-	
-	if(n==0)
-	return 1;
-	if(n==1)
-	return 0;
-	int odd=0;
-	int even=0;
-	while(n){
-		if(n & 1)
-		 ++odd;
-		n = n>>1;
-		if(n & 1)
-		 ++even;
-		n = n>>1;
-
-	}
-
-	return (isMultipleOf3(abs(odd-even)));
-
+    if(n<0)
+    n=-n;
+    
+    if(n==0)
+        return 1;
+    if(n==1)
+        return 0;
+    int odd=0;
+    int even=0;
+    
+    while(n)
+    {
+        if(n & 1)
+         ++odd;
+        
+        n = n>>1;
+        if(n & 1)
+         ++even;
+        
+        n = n>>1;
+    }
+    return (isMultipleOf3(abs(odd-even)));
 }
 
-int main(){
+int main()
+{
 
-cout<<isMultipleOf3(298431)<<endl;
-cout<<isMultipleOf3(331)<<endl;
-
+    cout<<isMultipleOf3(298431)<<endl;
+    cout<<isMultipleOf3(331)<<endl;
+    cout<<isMultipleOf3(256431)<<endl;
+    cout<<isMultipleOf3(3313561)<<endl;
+    cout<<isMultipleOf3(29843458991)<<endl;
+    cout<<isMultipleOf3(879695331)<<endl;
 }
