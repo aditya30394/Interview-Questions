0930003fabd60a0f50cd0ccee1af9b41c2625687
Aditya Kumar <aditya30394@gmail.com>
2018-07-10 23:20:30 +0530

    Configuring the coding style in Intellij

--- Java/src/IsMultipleOf3.java
+++ Java/src/IsMultipleOf3.java
@@ -3,30 +3,32 @@ Write an Efficient Method to Check if a Number is Multiple of 3
 
 Geeksforgeeks link - https://www.geeksforgeeks.org/write-an-efficient-method-to-check-if-a-number-is-multiple-of-3/
  */
+
 import static java.lang.Math.abs;
 
-public class IsMultipleOf3 {
+public class IsMultipleOf3
+{
     static boolean IsNMultipleOf3(int n)
     {
-        if(n<0)
-            n=-n;
-        if(n==0)
+        if (n < 0)
+            n = -n;
+        if (n == 0)
             return true;
-        if(n==1)
+        if (n == 1)
             return false;
 
-        int odd=0;
-        int even=0;
-        while(n!=0)
+        int odd = 0;
+        int even = 0;
+        while (n != 0)
         {
-            if((n&1)!=0)
+            if ((n & 1) != 0)
                 ++odd;
-            n=n>>1;
-            if((n&1)!=0)
+            n = n >> 1;
+            if ((n & 1) != 0)
                 ++even;
-            n=n>>1;
+            n = n >> 1;
         }
-        return IsNMultipleOf3(abs(odd-even));
+        return IsNMultipleOf3(abs(odd - even));
     }
 
     public static void main(String[] args)
--- Java/src/XPowerY.java
+++ Java/src/XPowerY.java
@@ -3,30 +3,50 @@
  *
  * Geeksforgeeks - https://www.geeksforgeeks.org/write-an-iterative-olog-y-function-for-powx-y/
  * */
-public class XPowerY {
+public class XPowerY
+{
+    public static void main(String[] args)
+    {
+        XPowerY test = new XPowerY();
+        System.out.println(test.PowerRecursive(2, -3));
+        System.out.println(test.PowerRecursive(3.8f, -5));
+        System.out.println(test.PowerRecursive(5.0f, 5));
+        System.out.println(test.PowerIterative(-2, 3));
+        System.out.println(test.PowerIterative(3, 4));
+    }
+
     /* Recursive Function to calculate x raised to the power n in O(log n)*/
-    public float PowerRecursive(float x, int n) {
-        if (n == 0) {
+    public float PowerRecursive(float x, int n)
+    {
+        if (n == 0)
+        {
             return 1;
         }
         float temp = PowerRecursive(x, n / 2);
-        if (n % 2 == 0) {
+        if (n % 2 == 0)
+        {
             return temp * temp;
-        } else {
-            if (n > 0) {
+        } else
+        {
+            if (n > 0)
+            {
                 return x * temp * temp;
-            } else {
+            } else
+            {
                 return (temp * temp) / x;
             }
         }
     }
 
     /* Iterative Function to calculate x raised to the power y in O(log y)*/
-    public long PowerIterative(int x, int y) {
+    public long PowerIterative(int x, int y)
+    {
         long ans = 1;
-        while (y > 0) {
+        while (y > 0)
+        {
             // If y is odd, multiply x with result
-            if (y % 2 == 1) {
+            if (y % 2 == 1)
+            {
                 ans = ans * x;
             }
             // y must be even now
@@ -34,15 +54,6 @@ public class XPowerY {
             x = x * x; // x = x*2
         }
         return ans;
-    }
-
-    public static void main(String[] args) {
-        XPowerY test = new XPowerY();
-        System.out.println(test.PowerRecursive(2, -3));
-        System.out.println(test.PowerRecursive(3.8f, -5));
-        System.out.println(test.PowerRecursive(5.0f, 5));
-        System.out.println(test.PowerIterative(-2, 3));
-        System.out.println(test.PowerIterative(3, 4));
     }
 }
 
