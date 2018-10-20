5ca13ec95515748a5339ce5f492f06281a16f27b
Aditya Kumar <aditya30394@gmail.com>
2018-10-20 14:56:55 -0500

    Added the code for smallest positive missing number

---
+++ C++/FirstMissingPositive.cpp
@@ -0,0 +1,50 @@
+/*
+First missing positive number
+
+Given an unsorted integer array, find the smallest missing positive integer.
+
+Example 1:
+
+Input: [1,2,0]
+Output: 3
+Example 2:
+
+Input: [3,4,-1,1]
+Output: 2
+Example 3:
+
+Input: [7,8,9,11,12]
+Output: 1
+Note:
+
+Your algorithm should run in O(n) time and uses constant extra space.
+
+Link: https://leetcode.com/explore/interview/card/top-interview-questions-hard/116/array-and-strings/832/
+
+Explanation:
+Put each number in its right place.
+
+For example:
+
+When we find 5, then swap it with A[4].
+
+At last, the first place where its number is not right, return the place + 1.
+
+*/
+
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& A) {
+        int n = A.size();
+        
+        for(int i = 0; i < n; ++ i)
+            while(A[i] > 0 && A[i] <= n && A[A[i] - 1] != A[i])
+                swap(A[i], A[A[i] - 1]);
+        
+        for(int i = 0; i < n; ++ i)
+            if(A[i] != i + 1)
+                return i + 1;
+        
+        return n + 1;
+    }
+};
