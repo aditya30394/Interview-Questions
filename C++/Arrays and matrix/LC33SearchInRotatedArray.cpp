3d8e36aca3f44b16f57ef5c9eb0ee89836640745
Aditya Kumar <aditya30394@gmail.com>
2019-01-21 23:40:47 -0600

    Added the solution to LC 33
    Explanation and multiple solution https://www.geeksforgeeks.org/search-an-element-in-a-sorted-and-pivoted-array/

--- C++/Arrays and matrix/LC33SearchInRotatedArray.cpp
+++ C++/Arrays and matrix/LC33SearchInRotatedArray.cpp
@@ -13,3 +13,43 @@ You may assume no duplicate exists in the array.
 Your algorithm's runtime complexity must be in the order of O(log n).
 */
 
+// Explanation is here: https://www.geeksforgeeks.org/search-an-element-in-a-sorted-and-pivoted-array/
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l=0, h=nums.size()-1;
+        int mid;
+        while(l<=h)
+        {
+            mid = l+(h-l)/2;
+            if(nums[mid]==target)
+            {
+                return mid;
+            }
+            else if(nums[l]<=nums[mid])//first half is sorted
+            {
+                if(nums[l]<=target and target<=nums[mid])
+                {
+                    h = mid-1;
+                }
+                else
+                {
+                    l = mid+1;
+                }
+            }
+            else
+            {
+                if(nums[mid]<=target and target<=nums[h])
+                {
+                    l = mid+1;
+                }
+                else
+                {
+                    h=mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+};
+
