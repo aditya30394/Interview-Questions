c4231f173cafb822565dec32d3945885d090730c
Aditya Kumar <aditya30394@gmail.com>
2019-01-27 15:34:58 -0600

    Added solution to LC 701 Insert into a Binary Search Tree

---
+++ C++/Trees/LC701BSTInsert.cpp
@@ -0,0 +1,116 @@
+/*
+701. Insert into a Binary Search Tree
+Medium
+
+Given the root node of a binary search tree (BST) and a value to be inserted into the tree, insert the value into the BST. Return the root node of the BST after the insertion. It is guaranteed that the new value does not exist in the original BST.
+
+Note that there may exist multiple valid ways for the insertion, as long as the tree remains a BST after insertion. You can return any of them.
+
+For example, 
+
+Given the tree:
+        4
+       / \
+      2   7
+     / \
+    1   3
+And the value to insert: 5
+You can return this binary search tree:
+
+         4
+       /   \
+      2     7
+     / \   /
+    1   3 5
+This tree is also valid:
+
+         5
+       /   \
+      2     7
+     / \   
+    1   3
+         \
+          4
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        
+        if(root==nullptr)
+        {
+            return new TreeNode(val);
+        }
+        
+        if(val > root->val)
+        {
+            root->right = insertIntoBST(root->right, val);
+        }
+        else
+        {
+            root->left = insertIntoBST(root->left, val);
+        }
+        return root;
+    }
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        TreeNode* newNode = new TreeNode(val);
+        if(root==nullptr)
+        {
+            return newNode;
+        }
+        
+        TreeNode* curr = root;
+        
+        while(curr)
+        {
+            if(val > curr->val)
+            {
+                if(curr->right)
+                {
+                    curr=curr->right;
+                }
+                else
+                {
+                    curr->right=newNode;
+                    break;
+                }
+            }
+            else
+            {
+                if(curr->left)
+                {
+                    curr=curr->left;
+                }
+                else
+                {
+                    curr->left=newNode;
+                    break;
+                }
+            }
+        }
+        
+        return root;
+    }
+};
