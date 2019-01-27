6db1b2cb99374f3e28f118a9d5c09994ab137918
Aditya Kumar <aditya30394@gmail.com>
2019-01-27 14:19:20 -0600

    Added the EPI problem 4.4 ClosestIntSameBitCount

---
+++ C++/Bit Magic/closest_int_same_weight.cc
@@ -0,0 +1,27 @@
+#include <stdexcept>
+#include "../../test_framework/generic_test.h"
+
+using std::invalid_argument;
+
+unsigned long long ClosestIntSameBitCount(unsigned long long x) {
+  const int bitSize = 64;
+  for(int i=0;i<bitSize-1;++i)
+  {
+      if(( (x>>i)&1) != ((x>>(i+1))&1))
+      {
+          x = x ^ ((1UL<<i) | (1UL << (i+1)));
+          return x;
+      }
+  }
+
+  // Throw error if all bits of x are 0 or 1.
+  throw invalid_argument("All bits are 0 or 1");
+}
+
+int main(int argc, char* argv[]) {
+  std::vector<std::string> args{argv + 1, argv + argc};
+  std::vector<std::string> param_names{"x"};
+  return GenericTestMain(args, "closest_int_same_weight.cc",
+                         "closest_int_same_weight.tsv", &ClosestIntSameBitCount,
+                         DefaultComparator{}, param_names);
+}
