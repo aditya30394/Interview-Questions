58df91e1f192c6669fa455b82afe4c96aef55de1
Aditya Kumar <aditya30394@gmail.com>
2019-02-12 16:09:07 -0600

    Added the problem LC380 Insert Delete GetRandom O(1)

---
+++ C++/Design Problems/LC380ConstOperations.cpp
@@ -0,0 +1,92 @@
+/*
+380. Insert Delete GetRandom O(1)
+Medium
+
+Design a data structure that supports all following operations in average O(1) time.
+
+insert(val): Inserts an item val to the set if not already present.
+remove(val): Removes an item val from the set if present.
+getRandom: Returns a random element from current set of elements. Each element must have the same probability of being returned.
+Example:
+
+// Init an empty set.
+RandomizedSet randomSet = new RandomizedSet();
+
+// Inserts 1 to the set. Returns true as 1 was inserted successfully.
+randomSet.insert(1);
+
+// Returns false as 2 does not exist in the set.
+randomSet.remove(2);
+
+// Inserts 2 to the set, returns true. Set now contains [1,2].
+randomSet.insert(2);
+
+// getRandom should return either 1 or 2 randomly.
+randomSet.getRandom();
+
+// Removes 1 from the set, returns true. Set now contains [2].
+randomSet.remove(1);
+
+// 2 was already in the set, so return false.
+randomSet.insert(2);
+
+// Since 2 is the only number in the set, getRandom always return 2.
+randomSet.getRandom();
+*/
+
+class RandomizedSet {
+    unordered_map<int,int> map;
+    vector<int> nums;
+public:
+    /** Initialize your data structure here. */
+    RandomizedSet() {
+        
+    }
+    
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    bool insert(int val) {
+        auto it = map.find(val);
+        if(it==map.end())
+        {
+            map[val]=nums.size();
+            nums.push_back(val);
+            return true;
+        }
+        return false;
+    }
+    
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    bool remove(int val) {
+        auto it = map.find(val);
+        
+        if(it==map.end())
+        {
+            return false;
+        }
+        
+        int loc = (*it).second;
+        if(loc<nums.size()-1)
+        {
+            // swap the element at the end
+            int last_element = nums[nums.size()-1];
+            nums[loc] = last_element;
+            map[last_element] = loc;
+        }
+        nums.erase(nums.end()-1);
+        map.erase(it);
+        return true;
+    }
+    
+    /** Get a random element from the set. */
+    int getRandom() {
+        return nums[rand()%nums.size()];
+    }
+};
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * bool param_1 = obj.insert(val);
+ * bool param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
